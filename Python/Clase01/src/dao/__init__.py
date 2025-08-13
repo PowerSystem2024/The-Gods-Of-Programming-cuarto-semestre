8c422be38c695fea21ebfd8e20574486de1fe1b3
Nahuel Ramirez <jnramirezlorca@gmail.com>
2025-08-13 20:58:36 -0300

    Primer ejercicio de python

---
+++ Python/Clase01/src/dao/__init__.py
@@ -0,0 +1,10 @@
+"""
+Módulo DAO (Data Access Objects)
+===============================
+
+Exporta todas las clases DAO del sistema
+"""
+
+from .usuario_dao import UsuarioDao
+
+__all__ = ['UsuarioDao']
